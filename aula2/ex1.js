c7098fc208181cf4be59c35ea36ebe2aa9b49923
Eliascolin <eliascolina.c@gmail.com>
2022-10-25 10:56:16 -0300

    feita tarefa aula 2

---
+++ aula2/ex1.js
@@ -0,0 +1,16 @@
+console.log ('hola mundo')
+
+
+let nome = "Elias"
+
+let Idade = (25)
+
+let usuarioAtivo = true
+
+console.log("Nome: ")
+console.log(nome)
+console.log("idade: ")
+console.log(Idade)
+console.log("Usuarioi esta ativo no sistema?")
+console.log(usuarioAtivo)
+
