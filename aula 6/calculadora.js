217b7bc1044ab1ceae74b4e2ae97422a45e0cf32
Eliascolin <eliascolina.c@gmail.com>
2022-10-26 22:59:41 -0300

    feita a aula 6

---
+++ aula 6/calculadora.js
@@ -0,0 +1,28 @@
+function adicionar (num1 = 0, num2 = 0){
+    const resultado = num1+num2;
+    return (resultado);
+}
+
+
+
+
+function subtraçao (num1 = 0, num2 = 0){
+    const resultado = num1-num2;
+    return resultado;
+}
+
+
+
+function multiplicaçao (num1 = 0, num2 = 0){
+    const resultado = num1*num2;
+    return resultado;
+}
+
+
+function diviçao (num1 = 0, num2 = 0){
+    const resultado = num1/num2;
+    return resultado;
+}
+
+ const retorno= adicionar(3,2)
+console.log ("..Teste de Operações / Calculadora .." + retorno);
