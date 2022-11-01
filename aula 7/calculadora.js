835d73e499656ddca85bd97d67605c4898bcb3c3
Eliascolin <eliascolina.c@gmail.com>
2022-11-01 10:57:20 -0300

    editata o arquivo js na pasta aula 7

---
+++ aula 7/calculadora.js
@@ -0,0 +1,52 @@
+
+function soma(num1,num2){
+    return num1+num2;
+}
+
+console.log ( 'resutado da suma =' , soma(7,3));
+
+function subtraçao(num1,num2){
+    return num1-num2;
+}
+
+console.log("resultado da subtraçao =", subtraçao(9,6))
+
+function multiplicaçao(num1,num2){
+    return num1*num2;
+}
+
+console.log("resultado da multiplicaçao =", multiplicaçao(3,3))
+
+function divisao(num1,num2){
+
+}
+
+function divisao(num1,num2){
+    return num1/num2;
+}
+
+
+console.log("resultado da divisao =", divisao(10,2))
+
+function divisaoInfinity(num1,num2){
+    return num1/num2;
+}
+
+console.log("testando divisao entre 0 =", divisaoInfinity(7,0))
+
+
+
+function quadradoDoNumero(numero = 5){
+
+return multiplicaçao(numero,numero);
+}
+
+console.log("resultado multiplicaçao ao quadrado", quadradoDoNumero(2))
+
+
+function mediaDeTresNumeros(num1,num2,num3){
+    return subtraçao(soma(num1,num2,num3));
+}
+
+
+console.log("resultado mediaDeTresNumeros", mediaDeTresNumeros(5,6,7))
