174ed4328715feafe6d494ea47311b5425986113
Eliascolin <eliascolina.c@gmail.com>
2022-11-10 17:27:21 -0300

    modificado

--- checkpoit/checkpoit.js
+++ checkpoit/checkpoit.js
@@ -5,35 +5,67 @@ let feijao = 12
 let brigadeiro = 8
 
 
-function microondas (prato,tempo = 0){
-     if(prato <= tempo) {
-        console.log("prato feito") 
-    }
 
 
-    else if(prato > tempo) {
+
+// function microondas (prato,tempo ){
+
+//     if  (prato= pipoca || macarrao ||  carne || feijao || brigadeiro  ){
+//         console.log("Prato inexistente")
+//     }
+
+//      else if(tempo < prato){
+//         console.log("tempo insuficiente") 
+//     }
+    
+//     else if(tempo  >= prato && tempo <= prato*2) {
+//         console.log("prato feito") 
+//     }
+//     else if(tempo >= prato*2 && tempo <= prato*3 ) 
+//     {console.log("qUEMOU")}
+
+//     else 
+//     {console.log("KABUUM")};
+// }
+
+   
+
+
+// microondas(kkk,50)
+
+
+    
+
+
+
+
+
+
+
+function microondas (prato,tempo ){
+
+
+    if  (prato == pipoca || macarrao ||  carne || feijao || brigadeiro  ){
+                console.log("Prato inexistente")
+            }
+
+     else if(tempo < prato){
         console.log("tempo insuficiente") 
     }
-  
-    else if(prato < 2* tempo   ){
-        console.log("queimo")
+    
+    else if(tempo  >= prato && tempo <= prato*2) {
+        console.log("prato feito") 
     }
-    // else if(prato < tempo*3  ){
-    //     console.log("kabumm")
-    // }
-    else{console.log("sem tempo")}
-    
+    else if(tempo >= prato*2 && tempo <= prato*3 ) 
+    {console.log("qUEMOU")}
+
+    else 
+    {console.log("KABUUM")};
 }
+
    
-    
 
 
-
-microondas(pipoca,21)
-
-
-
-
-
+microondas(brigadeiro,8)
 
 
