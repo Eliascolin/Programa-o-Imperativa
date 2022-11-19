d34ff330ddd49e1950a94cd13dd12f3d8bd880ae
Eliascolin <eliascolina.c@gmail.com>
2022-11-19 13:19:40 -0300

    feita a aula 12

---
+++ aula 12/aula12.js
@@ -0,0 +1,82 @@
+/*
+WHILE
+DO WHILW
+FOR
+
+REALIZAM TAREFAS EM MASSA, UO ATIVIDADES QUE SE REPETEM
+*/
+
+//WHILE --> ENQUANTO ALGUMA CONDIÇAO FOR VERDADEIRA, REPETIRA O CODIGO
+// SE FOR FALSA A REPETIÇAO E ENCERRADA 
+
+//parta parar um lop infinito control c
+
+//  let contador = 0;
+
+//  while(contador <= 10){
+//    //si o executaovel estiver primeiro daria um numero a mais  contador++;
+
+// console.log("repetiçao", contador);
+
+// //increment -- adicionar valor a una variable 
+// // forma tradicional =contador = contador + 3;
+// // forma simplificada = contador +=3;
+// contador++;
+// }
+
+
+//do while executa o codigo pe lo menos uma vez, ainda se for falsa 
+
+// contador = 101
+
+// do{
+//     console.log("contador..", contador);
+//     contador ++;
+// }while(false);
+
+
+
+//for 
+/*
+
+1 criterio inicial 
+
+2 criterio de parada 
+
+3 de quanto em quanto va ser executado 
+
+\n \n quebra de linha 
+
+*/
+
+// contador.log("iniciando contagem com for...\n \n")
+    //   1           2  
+// for(let contador = 0 ; contador <= 10 ; contador++){
+//     console.log("contador...",contador);
+// }
+
+// decremento 
+
+
+// for (let contador = 10 ; contador >= 0 ; contador--){
+//     console.log("contador...",contador);
+// }
+
+
+// na pratica 
+
+
+function soma(numeros){
+    return num1+num2;
+}
+
+// utilizar as estruturas de repetiçao para percorrer arrays
+
+
+ //             0  1 2 3  4
+let numeros = [26,10,7,14,73]; //130
+
+numeros,length// o numeros de elementos do array
+
+
+for(let indice = 0 ; indice < numeros.length  ; indice++){}
