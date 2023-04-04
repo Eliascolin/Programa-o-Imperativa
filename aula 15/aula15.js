f57714a9f65eb0fea09d53a1660de1504fee34c6
Eliascolin <eliascolina.c@gmail.com>
2023-04-03 21:58:52 -0300

    inicio checkpoint 2

---
+++ aula 15/aula15.js
@@ -0,0 +1,1 @@
+dgçflsdm
---
+++ checkpint2/checkponit2.js
@@ -0,0 +1,47 @@
+//https://docs.google.com/document/d/1eogP8YTM-zfPFqy4MMadArEM314Dxvq1/edit
+//https://docs.google.com/forms/d/e/1FAIpQLSfO1neD4NApOGQHVobOe-w4-YNE0fUjMk5Q99guFGc8qFGHwA/viewform
+
+
+
+//Crie uma Classe Aluno que tenha como atributos:
+ //nome (string), quantidade de faltas (number) e notas (array de números). 
+
+
+
+ /*
+ 
+ 
+ class aluno {
+
+    constructor (nome,faltas,notas){
+
+        this.nome = nome;
+
+        this.faltas = faltas;
+
+        this,notas = notas
+
+    }
+
+ }*/
+
+
+
+ /*Na Classe Aluno crie o método calcularMedia que retorna a média de suas notas. 
+ Também terá um método chamado faltas,
+ que simplesmente aumenta o número de faltas em 1. Crie alguns alunos para testar a sua Classe.*/
+
+
+ class aluno {
+
+    constructor (nome,faltas,notas){
+
+        this.nome = nome;
+
+        this.faltas = faltas;
+
+        this,notas = notas}
+
+        calcularMedia
+
+    }
