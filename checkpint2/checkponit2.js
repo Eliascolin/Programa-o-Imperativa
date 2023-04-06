0bc8bcfd2c2a2a5adb5dcfe87f639d9b0d43fb3c
Eliascolin <eliascolina.c@gmail.com>
2023-04-06 17:18:07 -0300

    continuando a aula

--- checkpint2/checkponit2.js
+++ checkpint2/checkponit2.js
@@ -8,38 +8,122 @@
 
 
 
- /*
+
  
  
- class aluno {
+ class Aluno {
 
-    constructor (nome,faltas,notas){
+                 constructor (nome,faltas,notas){
 
-        this.nome = nome;
+                 this.nome = nome,
 
-        this.faltas = faltas;
+                this.faltas = faltas,
 
-        this,notas = notas
+                 this.notas = notas
 
+       }
+
+
+    /*Na Classe Aluno crie o método calcularMedia que retorna a média de suas notas. 
+    Também terá um método chamado faltas, que simplesmente
+     aumenta o número de faltas em 1. Crie alguns alunos para testar a sua Classe.*/
+
+
+ 
+
+    
+ /*terá um método chamado faltas, que simplesmente aumenta o número de faltas em 1. Crie alguns alunos para testar a sua Classe.*/
+
+ 
+
+ }
+
+
+ const novoAluno = {
+
+ alunoNovo:[],
+
+adicionarAluno(nome,faltas,notas){
+const estudante = new Aluno (nome,faltas,notas)
+this.alunoNovo.push(estudante)
+},
+
+
+/*Na Classe Aluno crie o método calcularMedia que retorna a média de suas notas. 
+    Também terá um método chamado faltas, que simplesmente
+     aumenta o número de faltas em 1. Crie alguns alunos para testar a sua Classe.*/
+
+     calcularMedia(mediaNotas){
+        
+        for(let meidia of this.alunoNovo){
+
+       if(meidia.aluno === mediaNotas){
+       
+        let calculo = this.notas
+
+        let medias = 0;
+
+         for(var i = 0; i < calculo.length; i++) {
+    
+           let resultado = medias += calculo[i]
+
+           return console.log(resultado)
+
+     }
+
+
+    
+     }
+
+
+     }
+
+
+ }
+
+
+
+ faltas(mediaFatas){
+
+
+    for(let meidia of this.alunoNovo){
+
+
+        if(meidia.aluno === mediaFatas ){
+          
+            
+        
+            const calculo = this.faltas
+            for( var i = mediaFatas; i = mediaFatas; i++)
+
+               console.log (calculo)
+
+        }
     }
 
- }*/
+
+ }
 
 
 
- /*Na Classe Aluno crie o método calcularMedia que retorna a média de suas notas. 
- Também terá um método chamado faltas,
- que simplesmente aumenta o número de faltas em 1. Crie alguns alunos para testar a sua Classe.*/
+ }
 
 
- class aluno {
 
-    constructor (nome,faltas,notas){
 
-        this.nome = nome;
 
-        this.faltas = faltas;
 
-        this,notas = notas}
+// Crie alguns alunos para testar a sua Classe.
 
-    }
+
+novoAluno.adicionarAluno("aluno1",4,[1,2,3,4,5])
+
+novoAluno.adicionarAluno("aluno2",2,[5,4,])
+
+novoAluno.adicionarAluno("aluno3",5,[7,2,3])
+
+console.table(novoAluno.alunoNovo)
+
+const faltasCalculo = novoAluno.calcularMedia("aluno1")
+
+console.log(faltasCalculo)
